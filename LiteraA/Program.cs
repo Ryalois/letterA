523c72060859471cca4d1d17d317c5072bd8b916
JA <JA@DESKTOP-NF8JU3C>
2024-07-01 15:19:22 +0200

    Add project files.

---
+++ LiteraA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+String l;
+int aCount = 0;
+try
+{
+    StreamReader reader = new StreamReader("C:/test/test_kon_bal.txt");
+
+    l = reader.ReadLine();
+    while( l != null )
+    {
+        for( int i=0; i<l.Length; i++ )
+        {
+            if (l[i] == 'a') aCount++;
+        }
+        l = reader.ReadLine();
+    }
+    Console.WriteLine(aCount);
+}
+catch(Exception e)
+{
+    Console.WriteLine(e);
+}
